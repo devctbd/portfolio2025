1af365a17dd5b23d2dface50e1b2cf251148c9ab
devctbd <devctme@gmail.com>
2025-07-16 15:06:06 +0600

    add about page animation with framer moton

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
-import { Slider } from "@/components/ui/slider";
 import Link from "next/link";
 import {
   Breadcrumb,
@@ -17,7 +16,7 @@ import { motion } from "framer-motion";
 
 const About = () => {
   return (
-    <div className="min-h-screen container mx-auto px-4 md:px-0">
+    <div className="min-h-screen container mx-auto px-4 md:px-0 ">
       <Breadcrumb className="py-5">
         <BreadcrumbList>
           <BreadcrumbItem>
@@ -29,17 +28,13 @@ const About = () => {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      <h2 className="text-3xl lg:text-4xl font-bold mb-10">About Me</h2>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 ">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-          className="flex flex-col gap-5"
-        >
-          <Image
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div className="flex flex-col py-5 justify-center">
+          <motion.img
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
             src="/minarul.jpg"
             alt="Profile"
             width={120}
@@ -47,91 +42,135 @@ const About = () => {
             className="object-cover rounded-full"
           />
 
-          <div className="mt-5 flex flex-col gap-5">
-            <h2 className="text-2xl font-bold">Minarul Islam</h2>
-            <p className="text-gray-700 dark:text-gray-400">
+          <div className="mt-5 flex flex-col gap-5 max-w-prose">
+            <motion.h2
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 1 }}
+              className="text-2xl font-bold"
+            >
+              Minarul Islam
+            </motion.h2>
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 1.3 }}
+              className="text-gray-700 dark:text-gray-400"
+            >
               I am a skilled JavaScript developer with a strong background in
               developing web applications. I have experience with popular
               frameworks such as React and Next.js. I also have experience with
               Node.js and Express. I am always looking to learn new technologies
               and expand my skillset.
-            </p>
-            <div className="flex gap-2">
+            </motion.p>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 1.6 }}
+              className="flex gap-2"
+            >
               <Button variant="outline">Download CV</Button>
               <Button variant="outline">
                 <Link href="/contact">Hire Me</Link>
               </Button>
-            </div>
+            </motion.div>
           </div>
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-          className="flex flex-col gap-4"
-        >
-          <h2 className="text-3xl font-bold mt-10 mb-5">Skills</h2>
-          <p className="text-gray-700 dark:text-gray-400">Frontend</p>
-          <div className="flex flex-col gap-4">
-            <div className="flex items-center gap-2">
-              <h2 className="text-2xl font-bold">HTML</h2>
-              <p className="text-gray-700 dark:text-gray-400">96%</p>
-              <Slider defaultValue={[96]} />
-            </div>
-            <div className="flex items-center gap-2">
-              <h2 className="text-2xl font-bold">CSS</h2>
-              <p className="text-gray-700 dark:text-gray-400">90%</p>
-              <Slider defaultValue={[90]} />
-            </div>
-            <div className="flex items-center gap-2">
-              <h2 className="text-2xl font-bold">JavaScript</h2>
-              <p className="text-gray-700 dark:text-gray-400">65%</p>
-              <Slider defaultValue={[65]} />
-            </div>
-            <div className="flex items-center gap-2">
-              <h2 className="text-2xl font-bold">React</h2>
-              <p className="text-gray-700 dark:text-gray-400">75%</p>
-              <Slider defaultValue={[75]} />
-            </div>
-
-            <p className="text-gray-700 dark:text-gray-400 uppercase">cms</p>
-            <div className="flex items-center gap-2">
-              <h2 className="text-2xl font-bold">Wordpress</h2>
-              <p className="text-gray-700 dark:text-gray-400">80%</p>
-              <Slider defaultValue={[80]} />
-            </div>
-            <div className="flex items-center gap-2">
-              <h2 className="text-2xl font-bold">Sanity</h2>
-              <p className="text-gray-700 dark:text-gray-400">65%</p>
-              <Slider defaultValue={[65]} />
-            </div>
-            <p className="text-gray-700 dark:text-gray-400 uppercase">
-              framework
-            </p>
-            <div className="flex items-center gap-2">
-              <h2 className="text-2xl font-bold">Next.js</h2>
-              <p className="text-gray-700 dark:text-gray-400">85%</p>
-              <Slider defaultValue={[85]} />
-            </div>
-            <div className="flex items-center gap-2">
-              <h2 className="text-2xl font-bold">Node.js</h2>
-              <p className="text-gray-700 dark:text-gray-400">70%</p>
-              <Slider defaultValue={[70]} />
-            </div>
-            <div className="flex items-center gap-2">
-              <h2 className="text-2xl font-bold">Express.js</h2>
-              <p className="text-gray-700 dark:text-gray-400">60%</p>
-              <Slider defaultValue={[60]} />
-            </div>
-            <div className="flex items-center gap-2">
-              <h2 className="text-2xl font-bold">Laravel</h2>
-              <p className="text-gray-700 dark:text-gray-400">87%</p>
-              <Slider defaultValue={[87]} />
-            </div>
-          </div>
-        </motion.div>
+        </div>
+        {/* right grid */}
+        <div className="py-5">
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1.8 }}
+            className="text-3xl lg:text-4xl font-bold mb-10"
+          >
+            Skills
+          </motion.h2>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1 }}
+            className="w-full grid grid-cols-1 lg:grid-cols-2 gap-4"
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 2 }}
+              className="border bg-accent p-4 rounded-md"
+            >
+              <h2 className="text-xl font-bold">Frontend</h2>
+              <div className="flex flex-col gap-2 mt-4">
+                <p className="text-gray-700 dark:text-gray-400">React</p>
+                <p className="text-gray-700 dark:text-gray-400">Next.js</p>
+                <p className="text-gray-700 dark:text-gray-400">Vite</p>
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 2.5 }}
+              className="border bg-accent p-4 rounded-md"
+            >
+              <h2 className="text-xl font-bold">Backend</h2>
+              <div className="flex flex-col gap-2 mt-4">
+                <p className="text-gray-700 dark:text-gray-400">Node.js</p>
+                <p className="text-gray-700 dark:text-gray-400">Express</p>
+                <p className="text-gray-700 dark:text-gray-400">PHP</p>
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 2.7 }}
+              className="border bg-accent p-4 rounded-md"
+            >
+              <h2 className="text-xl font-bold">Framework</h2>
+              <div className="flex flex-col gap-2 mt-4">
+                <p className="text-gray-700 dark:text-gray-400">Next.js</p>
+                <p className="text-gray-700 dark:text-gray-400">Laravel</p>
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 2.8 }}
+              className="border bg-accent p-4 rounded-md"
+            >
+              <h2 className="text-xl font-bold">Version Control</h2>
+              <div className="flex flex-col gap-2 mt-4">
+                <p className="text-gray-700 dark:text-gray-400">Git</p>
+                <p className="text-gray-700 dark:text-gray-400">GitHub</p>
+                <p className="text-gray-700 dark:text-gray-400">GitLab</p>
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 3 }}
+              className="border bg-accent p-4 rounded-md"
+            >
+              <h2 className="text-xl font-bold">Version Control</h2>
+              <div className="flex flex-col gap-2 mt-4">
+                <p className="text-gray-700 dark:text-gray-400">Git</p>
+                <p className="text-gray-700 dark:text-gray-400">GitHub</p>
+                <p className="text-gray-700 dark:text-gray-400">GitLab</p>
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5, delay: 3.3 }}
+              className="border bg-accent p-4 rounded-md"
+            >
+              <h2 className="text-xl font-bold">Database</h2>
+              <div className="flex flex-col gap-2 mt-4">
+                <p className="text-gray-700 dark:text-gray-400">MySQL</p>
+                <p className="text-gray-700 dark:text-gray-400">MongoDB</p>
+                <p className="text-gray-700 dark:text-gray-400">PostgreSQL</p>
+              </div>
+            </motion.div>
+          </motion.div>
+        </div>
       </div>
     </div>
   );
